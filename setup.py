68a448a621109506bebcec5b520845f5a8b96aa1
abhaykatheria <abhay.katheria1998@gmail.com>
2021-01-30 00:06:42 +0530

    First Commit - made Boilerplate module
    As of now this does not have any dependency.
    happy developing :)
    
    Signed-off-by: abhaykatheria <abhay.katheria1998@gmail.com>

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='json2tree',
+    version='0.1.0',    
+    description='Python package to create html tree view for json files.',
+    url='https://github.com/abhaykatheria/json2tree',
+    author='Abhay Katheria and Mithilesh tiwari',
+    author_email='abhay.katheria1998@gmail.com',
+    license='MIT',
+    packages= find_packages(),
+    
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.7+',
+    ],
+)
