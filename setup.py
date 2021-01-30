92b27fc36665fbd600229e591cf0fedc2bb9bc7a
abhaykatheria <abhay.katheria1998@gmail.com>
2021-01-30 23:35:42 +0530

    Command line setup complete
    json2tree can be called from shell
    
    Signed-off-by: abhaykatheria <abhay.katheria1998@gmail.com>

--- setup.py
+++ setup.py
@@ -16,4 +16,8 @@ setup(
         'Intended Audience :: Science/Research',
         'Programming Language :: Python :: 3.7+',
     ],
+    entry_points={
+    "console_scripts": ["json2tree=json2tree.__main__:main"]
+    },
+
 )
